9973e5ef3cd5f1c2c78e3414f483831e666b18c2
Omkar Chavan <40963275+Omkarprakashchavan@users.noreply.github.com>
2024-10-02 13:42:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,56 +20,6 @@ from os.path import isfile, join
 import requests
 import json
 
-api_url = 'https://api.github.com/graphql'
-# github_token = os.environ['GITHUB_APP_TOKEN']
-organisation = 'Omkarprakashchavan'
-repositories = []
-# headers = {
-#     'Authorization': f'Bearer {github_token}',
-#     'Content-Type': 'application/json'
-# }
-
-logger: Union[logging.Logger, None] = None
-gh_obj = None
-topdir = os.path.dirname(os.path.abspath(sys.argv[0]))
-logdir = f'{topdir}/logdir'
-file_name_pattern='managed-ci'
-
-versioned_ci_repo = f'{os.path.dirname(__file__)}/../tarun-repo-1'
-repo_path = versioned_ci_repo
-file_path = 'workflow-deployment.yaml'
-print(f'printing version ci repo name {versioned_ci_repo} {repo_path}')
-repo = git.Repo(repo_path)
-try:
-  main_branch = repo.heads.main
-  print(f'main branch name {main_branch}')
-except AttributeError:
-  raise ValueError("The repository does not have a branch named 'main'.")
-
-latest_commit_sha = main_branch.commit.hexsha
-second_top_commit = get_second_top_commit(repo_path)
-print(f"Latest commit SHA of 'main': {latest_commit_sha}")
-try:
-  file_commit_sha = get_file_content_from_commit(repo, latest_commit_sha, file_path)
-  print(f"Commit SHA of '{file_path}' in the latest commit: {file_commit_sha}")
-except ValueError:
-  print(f"File '{file_path}' does not exist in the latest commit {latest_commit_sha}")
-
-try:
-  content_old = get_file_content_from_commit(repo, second_top_commit, file_path)
-  content_new = get_file_content_from_commit(repo, latest_commit_sha, file_path)
-except ValueError as e:
-  print(e)
-
-dict_old = load_yaml(content_old)
-dict_new = load_yaml(content_new)
-# Extract repositories data from dict1 and dict2
-repositories1 = dict_old['modules'][0]['repositories']
-repositories2 = dict_new['modules'][0]['repositories']
-
-changed_repositories = compare_repositories(repositories1, repositories2)
-print(f'Changed repositories: {changed_repositories}')
-
 def load_yaml(file_content):
     """Read workflow-deployment.yaml content into a dictionary."""
     return yaml.safe_load(file_content)
@@ -121,3 +71,53 @@ def compare_repositories(repo_list1, repo_list2):
     if not changes['repositories']:
         return {}
     return changes
+
+api_url = 'https://api.github.com/graphql'
+# github_token = os.environ['GITHUB_APP_TOKEN']
+organisation = 'Omkarprakashchavan'
+repositories = []
+# headers = {
+#     'Authorization': f'Bearer {github_token}',
+#     'Content-Type': 'application/json'
+# }
+
+logger: Union[logging.Logger, None] = None
+gh_obj = None
+topdir = os.path.dirname(os.path.abspath(sys.argv[0]))
+logdir = f'{topdir}/logdir'
+file_name_pattern='managed-ci'
+
+versioned_ci_repo = f'{os.path.dirname(__file__)}/../tarun-repo-1'
+repo_path = versioned_ci_repo
+file_path = 'workflow-deployment.yaml'
+print(f'printing version ci repo name {versioned_ci_repo} {repo_path}')
+repo = git.Repo(repo_path)
+try:
+  main_branch = repo.heads.main
+  print(f'main branch name {main_branch}')
+except AttributeError:
+  raise ValueError("The repository does not have a branch named 'main'.")
+
+latest_commit_sha = main_branch.commit.hexsha
+second_top_commit = get_second_top_commit(repo_path)
+print(f"Latest commit SHA of 'main': {latest_commit_sha}")
+try:
+  file_commit_sha = get_file_content_from_commit(repo, latest_commit_sha, file_path)
+  print(f"Commit SHA of '{file_path}' in the latest commit: {file_commit_sha}")
+except ValueError:
+  print(f"File '{file_path}' does not exist in the latest commit {latest_commit_sha}")
+
+try:
+  content_old = get_file_content_from_commit(repo, second_top_commit, file_path)
+  content_new = get_file_content_from_commit(repo, latest_commit_sha, file_path)
+except ValueError as e:
+  print(e)
+
+dict_old = load_yaml(content_old)
+dict_new = load_yaml(content_new)
+# Extract repositories data from dict1 and dict2
+repositories1 = dict_old['modules'][0]['repositories']
+repositories2 = dict_new['modules'][0]['repositories']
+
+changed_repositories = compare_repositories(repositories1, repositories2)
+print(f'Changed repositories: {changed_repositories}')
